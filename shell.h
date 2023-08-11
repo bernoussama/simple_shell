bdbdbe3aa89bf07af2a541611c97142f18937193
Marouane Asmiyen <123839250+MASMIYEN@users.noreply.github.com>
2023-08-11 21:37:32 +0100

    Update shell.h

--- shell.h
+++ shell.h
@@ -19,5 +19,13 @@ void _puts(char *str);
 int _putchar(char c);
 ssize_t _getline(char **lineptr, size_t *n, FILE *stream);
 
+/* STRINGS FUNCTIONS */
+char *_strcat(char *dest, char *src);
+int _strlen(char *s);
+int _strcmp(char *s1, char *s2);
+char *_strdup(char *str);
+char *_strncpy(char *dest, char *src, int n);
+char *_strcpy(char *dest, char *src);
+
 
 #endif
