166aa82cf73339ae36e273c7b41e08dde7d60c42
Marouane Asmiyen <123839250+MASMIYEN@users.noreply.github.com>
2023-08-12 21:25:01 +0100

    Update shell.h

--- shell.h
+++ shell.h
@@ -4,12 +4,12 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
-
 #include <string.h>
+
 #include <sys/types.h>
 #include <sys/wait.h>
-
 #include <sys/stat.h>
+
 #include <signal.h>
 #include <stddef.h>
 #include <errno.h>
