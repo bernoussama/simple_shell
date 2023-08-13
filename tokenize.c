9a66e7446c23345fbd78fc01f1cb0fa8009931e1
Marouane Asmiyen <123839250+MASMIYEN@users.noreply.github.com>
2023-08-13 22:05:58 +0100

    Update tokenize.c

--- tokenize.c
+++ tokenize.c
@@ -16,3 +16,23 @@ int _splitter(char *s)
   }
   return (count);
 }
+/**
+ * _cmpath - finds the value of PATH in environ
+ * @str1: pointer to "PATH"
+ * @str2: pointer to environ string
+ *
+ * Return: 0 on success || +1 if no PATH variable in environ
+ */
+int _cmpath(const char *str1, const char *str2)
+{
+  int i = 0;
+  while (str2[i] != '=')
+  {
+    if (str1[i] != str2[i])
+	{
+      return -1;
+    }
+    i++;
+  }
+  return 0;
+}
