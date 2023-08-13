4a7a552c236f374e5502fe8bab39d84c9ff17632
Marouane Asmiyen <123839250+MASMIYEN@users.noreply.github.com>
2023-08-13 22:10:13 +0100

    Update mem.c

--- mem.c
+++ mem.c
@@ -2,11 +2,11 @@
 /**
  * _memset - fills n bytes of memory
  *
- * @s: pointer to start of memory area
+ * @s: pointer to start of the memory area
  * @b: constant byte to fill with
  * @n: num bytes to fill in
  *
- * Return: pointer to beginning of memory area s
+ * Return: pointer to the beginning of memory area s
  */
 char *_memset(char *s, char b, unsigned int n)
 {
