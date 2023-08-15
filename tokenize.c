08902e8a586a9a03fdb0739317effed7dae21bee
Marouane Asmiyen <123839250+MASMIYEN@users.noreply.github.com>
2023-08-15 04:55:16 +0100

    Update tokenize.c

--- tokenize.c
+++ tokenize.c
@@ -19,3 +19,27 @@ int _cmpath(const char *str1, const char *str2)
   }
   return 0;
 }
+/**
+ * _cmpcat - concatenates 3 str to create a full PATH
+ * @str: static array to save the result of the concatenation
+ * @av: pointer to command string entered by USER
+ * @token: pointer to 1st part of a directory in PATH
+ *
+ * Return: pointer to the complete path to the command
+ */
+
+char *_cmpcat(char *str, char **av, char *token)
+{
+	int len, len1, len2;
+
+	_memset(str, 0, 256);
+	len1 = _strlen(token);
+	len2 = _strlen(av[0]);
+	len = len1 + len2 + 2;
+	_strcat(str, token);
+	_strcat(str, "/");
+	_strcat(str, av[0]);
+	str[len - 1] = '\0';
+
+	return (str);
+}
