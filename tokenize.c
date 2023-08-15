af61b16a47145c651a0947cca22d8f23b9eb79fe
Marouane Asmiyen <123839250+MASMIYEN@users.noreply.github.com>
2023-08-15 04:51:29 +0100

    Update tokenize.c

--- tokenize.c
+++ tokenize.c
@@ -1,23 +1,5 @@
 #include "shell.h"
 /**
- * _splitter - counts the number of paths in PATH
- * @s: pointer to PATH string
- *
- * Return: number of directories to check in PATH
- */
-int _splitter(char *s)
-{
-  int i, count = 0;
-  for (i = 0; s[i]; i++)
-  {
-    if (s[i] != ':' && (i == 0 || s[i - 1] != ':'))
-	{
-      count++;
-    }
-  }
-  return (count);
-}
-/**
  * _cmpath - finds the value of PATH in environ
  * @str1: pointer to "PATH"
  * @str2: pointer to environ string
